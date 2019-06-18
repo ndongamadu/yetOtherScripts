50002d8d6be8b202ec264c4c59ccd060f4c3be0e
ndongamadu <ndongamadou@gmail.com>
2019-06-18 11:57:49 +0000

    ipc info data to hdx script

---
+++ script-ipc-data-graber.py
@@ -0,0 +1,41 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.support.ui import WebDriverWait
+country = 'DRC'
+drc = "http://www.ipcinfo.org/ipc-country-analysis/details-map/en/c/1151753/"
+#caf = "http://www.ipcinfo.org/ipc-country-analysis/details-map/en/c/1151724/"
+# yemen = "http://www.ipcinfo.org/ipc-country-analysis/details-map/en/c/1151858/"
+
+drv = "/Users/amadu/.wdm/chromedriver/74.0.3729.6/mac64/chromedriver"
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(drv)
+driver.get(drc)
+driver.find_element_by_id('poestab').click()
+# time.sleep(10)
+# driver.switch_to.frame(driver.findElements(By.tagName("iframe"))).get(0)
+driver.switch_to.frame(1)
+page_source = driver.page_source
+soupe = BeautifulSoup(page_source, 'lxml')
+tab = soupe.find('table', id='population_data_3')
+rows = tab.findAll('tr')
+nbRows = 0
+
+with open('%s-ipcData.csv' % country, 'w') as file:
+    writer = csv.writer(file, delimiter=',')
+    for r in rows:
+        currentCell = r.findAll('td')
+        data = []
+        for c in currentCell:
+            data.append(c.get_text())
+
+        if nbRows == 0:
+            writer.writerow(data)
+            nbRows += 1
+            # add hxl tags
+        else:
+            writer.writerow(data)
+print("====table content===")
+# print(tab)
